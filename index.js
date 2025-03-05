977f8df64252e10d0bd406a83ff934c3415a49e9
mohammad-aarif <arifbd354@gmail.com>
2025-03-05 09:32:11 +0600

    Furnique Server initialized

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+
+const app = express()
+const port = 5000
+
+app.get('/', (req, res)=>{
+    res.send('Furnique Home')
+})
+app.listen(port, () => {
+    console.log('Server Running');   
+})
